0e683e6ee1381b2d4afa3a89649afc281d22a408
Juan David <judaram98@gmail.com>
2023-08-31 11:53:07 -0600

    🛠️build:Creating routes and pages

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,17 @@
+import "../App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login/Login.tsx";
+import Dashboard from "../pages/Dashboard/Dashboard.tsx";
+
+function HtmlRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login></Login>} />
+        <Route path="/principal" element={<Dashboard></Dashboard>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default HtmlRoutes;
