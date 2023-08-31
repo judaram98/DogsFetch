183a3201d2c25740f2fef12bff7225236d9ccb75
Juan David <judaram98@gmail.com>
2023-08-31 17:10:28 -0600

    ✨feat: creating useFetch hook

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,42 +1,30 @@
 import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 import FetchLogo from "../../assets/Fetch_Logo.jpeg";
+import TextField from "../../components/TextField";
+import Button from "../../components/Button";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
+  const { fetchData } = useFetch();
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    setLoading(true);
-
-    try {
-      const response = await fetch(
-        "https://frontend-take-home-service.fetch.com/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name, email }),
-        }
-      );
-
-      if (response.ok) {
-        // Aquí podrías manejar la lógica de éxito
-        // onLoginSuccess();
-        console.log(response);
-      } else {
-        // Aquí podrías manejar la lógica de error
-        console.error("Error login in");
-      }
-    } catch (error) {
-      console.error("Error fetching", error);
-    } finally {
-      setLoading(false);
+    const response = await fetchData(
+      "https://frontend-take-home-service.fetch.com/auth/login",
+      "POST",
+      { name: name, email: email }
+    );
+    if (response.ok) {
+      navigate("principal");
     }
   };
+
   return (
-    <section className="w-full h-screen flex flex-col justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <img src={FetchLogo} alt="Logo" className="w-2/12" />
       <h1 className="text-3xl font-light text-[#300C39] -mt-4 tracking-widest">
         Login
@@ -49,14 +37,12 @@ const Login = () => {
           >
             Name:
           </label>
-          <input
+          <TextField
             type="text"
             id="name"
             value={name}
             placeholder="Enter your name"
-            onChange={(e) => setName(e.target.value)}
-            className="px-3 border border-neutral-300 tex-neutral-300 font-normal rounded-lg h-10 outline-[#FBA819]"
-            required
+            setValue={setName}
           />
         </div>
         <div className="flex flex-col">
@@ -66,25 +52,17 @@ const Login = () => {
           >
             Email:
           </label>
-          <input
+          <TextField
             type="email"
             id="email"
             value={email}
             placeholder="Enter your email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="px-3 border border-neutral-300 tex-neutral-300 font-normal rounded-lg h-10 outline-[#FBA819]"
-            required
+            setValue={setEmail}
           />
         </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="bg-[#FBA819] hover:bg-[#E1981F] text-white py-2 rounded-lg"
-        >
-          Iniciar Sesión
-        </button>
+        <Button type="submit" value="Iniciar Sesión" />
       </form>
-    </section>
+    </div>
   );
 };
 
