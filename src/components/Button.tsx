b4ab327fbb9992f0e797c7d398edd0605c29a4fb
Juan David <judaram98@gmail.com>
2023-08-31 22:32:48 -0600

    🐛bug:Button correction

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ type, value }: ButtonProps) => {
   return (
     <button
       type={type}
-      className="bg-[#FBA819] hover:bg-[#E1981F] text-white py-2 rounded-lg"
+      className="bg-[#FBA819] hover:bg-[#E1981F] text-white py-2 rounded-lg w-full"
     >
       {value}
     </button>
