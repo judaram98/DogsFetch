3402a9ed1cce1971db2a70fe99bd449dee4515f7
Juan David <judaram98@gmail.com>
2023-09-01 08:27:55 -0600

    ✨feat:New dogCard code organization

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -9,7 +9,11 @@ interface DogCardProps {
 const DogCard = ({ img, name, age, zip, breed }: DogCardProps) => {
   return (
     <div className="w-1/6 border rounded-lg p-1 relative">
-      <img src={img} alt="Name" className="rounded-lg" />
+      <img
+        src={img}
+        alt="Name"
+        className="rounded-md object-cover w-full h-60"
+      />
       <p className=" mt-2 px-2 font-bold text-xl">{name}</p>
       <div className="flex justify-between items-center -mt-1 px-2">
         <p className="text-gray-400">{age} Years</p>
