db505eb1c82fa23c28101cf9d4aa3914040980e8
Juan David <judaram98@gmail.com>
2023-09-01 10:50:46 -0600

    ♻️refactor:Cleaning code

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,61 +1,28 @@
-import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 
-interface PaginationProps {
-  info: { pages: number } | null;
-  pageNumber: number;
-  setPageNumber: React.Dispatch<React.SetStateAction<number>>;
+interface Params {
+  totalResults: number;
+  setPage: (page: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({
-  info,
-  pageNumber,
-  setPageNumber,
-}) => {
-  const [width, setWidth] = useState<number>(window.innerWidth);
-
-  const updateDimension = () => {
-    setWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", updateDimension);
-    return () => window.removeEventListener("resize", updateDimension);
-  }, []);
-
-  const styles = `
-    @media (max-width: 768px) {
-      .next,
-      .prev {
-        display: none;
-      }
-      .pagination {
-        font-size: 14px;
-      }
-    }
-  `;
-
+const Pagination = ({ totalResults, setPage }: Params) => {
   return (
-    <>
-      <style>{styles}</style>
+    <div className="h-[10%] flex items-center justify-center">
       <ReactPaginate
-        className="pagination justify-content-center gap-4 my-4"
-        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-        nextLabel="Next"
-        previousLabel="Prev"
-        nextClassName="btn btn-primary next"
-        previousClassName="btn btn-primary prev"
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        marginPagesDisplayed={width < 576 ? 1 : 2}
-        pageRangeDisplayed={width < 576 ? 1 : 2}
-        activeClassName="active"
-        onPageChange={(data) => {
-          setPageNumber(data.selected + 1);
-        }}
-        pageCount={info?.pages || 1}
+        previousLabel={"< Prev"}
+        previousClassName="text-[#FBA819]"
+        nextLabel={"Next >"}
+        nextClassName="text-[#FBA819]"
+        breakLabel={"..."}
+        breakClassName={"text-neutral-300"}
+        pageCount={totalResults / 25}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={(e) => setPage(e.selected)}
+        containerClassName={"flex gap-4 text-neutral-500"}
+        activeClassName={"text-[#FBA819]"}
       />
-    </>
+    </div>
   );
 };
 
